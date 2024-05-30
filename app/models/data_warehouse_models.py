568ea37f8f571384b2b2b1d3aaa86066a7336c2d
David Antonio <happywybie@gmail.com>
2024-05-30 02:43:38 -0600

    ci: changes in cube variables

--- app/models/data_warehouse_models.py
+++ app/models/data_warehouse_models.py
@@ -5,7 +5,7 @@ Base = declarative_base()
 
 class DimensionPedido(Base):
     __tablename__ = 'DimensionPedido'
-    Pedidold = Column(Integer, primary_key=True, autoincrement=True)
+    PedidoId = Column(Integer, primary_key=True, autoincrement=True)
     NombreMetodoPago = Column(String(100), default='Unknown')
     DireccionPago = Column(String(100), default='Unknown')
     PrecioUnitario = Column(DECIMAL(10, 2), nullable=False)
@@ -36,7 +36,7 @@ class DimensionTiempo(Base):
 
 class DimensionCliente(Base):
     __tablename__ = 'DimensionCliente'
-    Clienteld = Column(Integer, primary_key=True, autoincrement=True)
+    ClienteId = Column(Integer, primary_key=True, autoincrement=True)
     Nombre = Column(String(100), default='Unknown')
     Apellido = Column(String(100), default='Unknown')
     Email = Column(String(100), default='Unknown')
@@ -47,7 +47,7 @@ class DimensionCliente(Base):
     Estado = Column(String(100), default='Unknown')
     CodigoPostal = Column(Integer, nullable=False)
     PuntosFidelidad = Column(Integer, nullable=False)
-    Fechalnicio = Column(DateTime)
+    FechaInicio = Column(DateTime)
     FechaFin = Column(DateTime)
 
 
@@ -60,42 +60,42 @@ class DimensionOfertas(Base):
     CodDescuento = Column(Integer, nullable=False)
     FechaLanzamiento = Column(DateTime)
     FechaCaducidad = Column(DateTime)
-    Fechalnicio = Column(DateTime)
+    FechaInicio = Column(DateTime)
     FechaFin = Column(DateTime)
 
 
 class DimensionAreaEnvio(Base):
     __tablename__ = 'DimensionAreaEnvio'
-    AreaEnviold = Column(Integer, primary_key=True, autoincrement=True)
+    AreaEnvioId = Column(Integer, primary_key=True, autoincrement=True)
     NombreArea = Column(String(100), default='Unknown')
     CostoEnvio = Column(DECIMAL(10, 2), nullable=False)
-    Fechalnicio = Column(DateTime)
+    FechaInicio = Column(DateTime)
     FechaFin = Column(DateTime)
 
 
 class DimensionProducto(Base):
     __tablename__ = 'DimensionProducto'
-    Productold = Column(Integer, primary_key=True, autoincrement=True)
+    ProductoId = Column(Integer, primary_key=True, autoincrement=True)
     Nombre = Column(String(100), default='Unknown')
     FechaAgregado = Column(DateTime)
     Dimensiones = Column(String(100), default='Unknown')
     Peso = Column(DECIMAL(10, 2), nullable=False)
     Nombrecategoria = Column(String(100), default='Unknown')
     NombreMarca = Column(String(100), default='Unknown')
-    Fechalnicio = Column(DateTime)
+    FechaInicio = Column(DateTime)
     FechaFin = Column(DateTime)
 
 
 class FactTableEnvio(Base):
     __tablename__ = 'FactTableEnvio'
     EnviosID = Column(Integer, primary_key=True, autoincrement=True)
-    Pedidold = Column(Integer, ForeignKey('DimensionPedido.Pedidold'))
+    PedidoId = Column(Integer, ForeignKey('DimensionPedido.PedidoId'))
     OfertasId = Column(Integer, ForeignKey('DimensionOfertas.Ofertasid'))
-    AreaEnviold = Column(Integer, ForeignKey('DimensionAreaEnvio.AreaEnviold'))
-    Clienteld = Column(Integer, ForeignKey('DimensionCliente.Clienteld'))
-    Productold = Column(Integer, ForeignKey('DimensionProducto.Productold'))
+    AreaEnvioId = Column(Integer, ForeignKey('DimensionAreaEnvio.AreaEnvioId'))
+    ClienteId = Column(Integer, ForeignKey('DimensionCliente.ClienteId'))
+    ProductoId = Column(Integer, ForeignKey('DimensionProducto.ProductoId'))
     DATAKEY = Column(Integer, ForeignKey('DimensionTiempo.DateId'))
-    Ubicacionld = Column(Integer, ForeignKey('DimensionUbicacion.UbicacionId'))
+    UbicacionId = Column(Integer, ForeignKey('DimensionUbicacion.UbicacionId'))
     Ofertasld = Column(Integer, ForeignKey('DimensionOfertas.Ofertasid'))
     EmpresaEnvio = Column(String(100), default='Unknown')
     MetodoEnvio = Column(String(100), default='Unknown')
