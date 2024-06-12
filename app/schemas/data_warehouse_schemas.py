bc6579bea1a502512f72cd6a6d042becb745e1ac
David Antonio <happywybie@gmail.com>
2024-06-12 01:10:24 -0600

    refactor: dw schema

--- app/schemas/data_warehouse_schemas.py
+++ app/schemas/data_warehouse_schemas.py
@@ -1,119 +1,115 @@
 from pydantic import BaseModel
-from datetime import datetime
 from typing import Optional
-
+from datetime import datetime
 
 class DimensionPedidoSchema(BaseModel):
-    PedidoId: Optional[int] = None
-    NombreMetodoPago: str = 'Unknown'
+    PedidoKey: Optional[int] = None
+    SourceKey: str
+    MetodoPago: str = 'Unknown'
     DireccionPago: str = 'Unknown'
     PrecioUnitario: float
     Cantidad: int
-    NombreEstado: str = 'Unknown'
-    FechaInicio: datetime
-    FechaFin: datetime
+    Estado: str = 'Unknown'
+    FechaPedido: datetime 
+    ValidFrom: datetime 
+    ValidTo: datetime 
+    LineageKey: int
 
     class Config:
         orm_mode = True
 
-
 class DimensionUbicacionSchema(BaseModel):
-    UbicacionId: Optional[int] = None
+    UbicacionKey: Optional[int] = None
+    SourceKey: str
     DireccionEnvio: str = 'Unknown'
     EstadoEnvio: str = 'Unknown'
     PaisEnvio: str = 'Unknown'
     CiudadEnvio: str = 'Unknown'
+    ValidFrom: datetime 
+    ValidTo: datetime 
+    LineageKey: int
 
     class Config:
         orm_mode = True
-
-
-class DimensionTiempoSchema(BaseModel):
-    DateId: Optional[int] = None
-    FechaEnvio: datetime
-    FechaEntrega: datetime
-    FechaPedido: datetime
-    FechaInicio: datetime
-    FechaFin: datetime
-
-    class Config:
-        orm_mode = True
-
 
 class DimensionClienteSchema(BaseModel):
-    ClienteId: Optional[int] = None
+    ClienteKey: Optional[int] = None
+    SourceKey: str
     Nombre: str = 'Unknown'
     Apellido: str = 'Unknown'
     Email: str = 'Unknown'
-    Telefono: int
+    Telefono: str = 'Unknown'
     Direccion: str = 'Unknown'
     Ciudad: str = 'Unknown'
     Pais: str = 'Unknown'
     Estado: str = 'Unknown'
     CodigoPostal: int
     PuntosFidelidad: int
-    FechaInicio: datetime
-    FechaFin: datetime
+    ValidFrom: datetime 
+    ValidTo: datetime 
+    LineageKey: int
 
     class Config:
         orm_mode = True
 
-
 class DimensionOfertasSchema(BaseModel):
-    Ofertasid: Optional[int] = None
+    OfertaKey: Optional[int] = None
+    SourceKey: str
     Nombre: str = 'Unknown'
     Descripcion: str = 'Unknown'
     Descuento: float
     CodDescuento: int
-    FechaLanzamiento: datetime
-    FechaCaducidad: datetime
-    FechaInicio: datetime
-    FechaFin: datetime
+    FechaLanzamiento: datetime 
+    FechaCaducidad: datetime 
+    ValidFrom: datetime 
+    ValidTo: datetime 
+    LineageKey: int
 
     class Config:
         orm_mode = True
-
 
 class DimensionAreaEnvioSchema(BaseModel):
-    AreaEnvioId: Optional[int] = None
-    NombreArea: str = 'Unknown'
+    AreaEnvioKey: Optional[int] = None
+    SourceKey: str
+    Area: str = 'Unknown'
     CostoEnvio: float
-    FechaInicio: datetime
-    FechaFin: datetime
+    ValidFrom: datetime 
+    ValidTo: datetime 
+    LineageKey: int
 
     class Config:
         orm_mode = True
-
 
 class DimensionProductoSchema(BaseModel):
-    ProductoId: Optional[int] = None
+    ProductoKey: Optional[int] = None
+    SourceKey: str
     Nombre: str = 'Unknown'
-    FechaAgregado: datetime
+    FechaAgregado: datetime 
     Dimensiones: str = 'Unknown'
     Peso: float
-    Nombrecategoria: str = 'Unknown'
+    NombreCategoria: str = 'Unknown'
     NombreMarca: str = 'Unknown'
-    FechaInicio: datetime
-    FechaFin: datetime
+    ValidFrom: datetime 
+    ValidTo: datetime 
+    LineageKey: int
 
     class Config:
         orm_mode = True
 
-
-class FactTableEnvioSchema(BaseModel):
-    EnviosID: Optional[int] = None
-    PedidoId: int
-    OfertasId: int
-    AreaEnvioId: int
-    ClienteId: int
-    ProductoId: int
-    DATEKEY: int
-    UbicacionId: int
-    OfertasId: int
+class FactEnvioSchema(BaseModel):
+    EnvioKey: Optional[int] = None
+    PedidoKey: int
+    OfertaKey: int
+    AreaEnvioKey: int
+    ClienteKey: int
+    ProductoKey: int
+    UbicacionKey: int
     EmpresaEnvio: str = 'Unknown'
     MetodoEnvio: str = 'Unknown'
-    ValidFrom: datetime
-    ValidTo: datetime
+    FechaEnvio: datetime 
+    FechaEntrega: datetime 
+    SourcePedidoKey: int
+    LineageKey: int
 
     class Config:
         orm_mode = True
