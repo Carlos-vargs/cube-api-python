b89aca76d2aca70148e2d415d6ffff897ddba063
David Antonio <happywybie@gmail.com>
2024-06-15 13:17:12 -0600

    ci: add middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from .api.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="API del Data Warehouse de TecnoNic")
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
